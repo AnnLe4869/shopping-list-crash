cc0b1057e99091e121bff14eada1cdd1e9805b24
Anh Le <kunquan2345@gmail.com>
2020-01-15 04:10:17 -0800

    Tweak file a little bit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>Hello world</header>
+      <h1>Hello world</h1>
     </div>
   );
 }
