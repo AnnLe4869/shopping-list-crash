94878e444f91e1a0fb87dcb19ca65fd51f73887e
Anh Le <kunquan2345@gmail.com>
2020-01-21 01:11:29 -0800

    Fourth attempt to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
