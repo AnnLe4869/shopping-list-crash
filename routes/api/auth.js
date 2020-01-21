f79fc2986d4f72e2fd3e99d52bb9df6092bdbaa1
Anh Le <kunquan2345@gmail.com>
2020-01-21 05:13:46 -0800

    Start set up authentication routes
    - Include in the JWT token when user register

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,31 +20,9 @@ router.post("/", async (req, res) => {
     }
     // Check for existed user
     const existedUser = await User.findOne({ email });
-    if (existedUser) {
-      return res.status(400).json({ message: "User already exited" });
+    if (!existedUser) {
+      return res.status(400).json({ message: "User not exited" });
     }
-    // If not existed yet
-    const hashPassword = await bcrypt.hash(password, 12);
-    const newUser = await User.create({ name, email, password: hashPassword });
-    const token = await jwt.signAsync(
-      {
-        id: newUser._id,
-        name: newUser.name,
-        email: newUser.email
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: 10 * 60
-      }
-    );
-    return res.status(200).json({
-      user: {
-        id: newUser._id,
-        name: newUser.name,
-        email: newUser.email
-      },
-      token: token
-    });
   } catch (err) {
     console.error(err);
   }
